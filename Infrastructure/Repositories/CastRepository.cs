d9407a467ad195d26a7360df4bead2242831ca30
akiman410 <booshkimani@comcast.net>
2022-03-10 13:32:47 -0500

    CreateInterface and Classes

---
+++ Infrastructure/Repositories/CastRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Contracts.Repositories;
+using ApplicationCore.Enities;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CastRepository : EfRepository<Cast>, ICastRepository
+    {
+        public CastRepository(MovieShopDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
